299e123e18d13f35d499695e671d447fd456209a
Derek Shaheen <dshaheen@css.edu>
2020-02-22 16:15:25 -0600

    Completed Practice 6

--- app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
+++ app/src/main/java/css/cis3334/intentsperformances/MainActivity.java
@@ -1,6 +1,8 @@
 package css.cis3334.intentsperformances;
 
 import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.net.Uri;
 import android.provider.CalendarContract;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
@@ -9,10 +11,13 @@ import android.widget.Button;
 import android.widget.TextView;
 
 import java.util.GregorianCalendar;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
 
     Button btn1Web, but1Calendar, btn1Map;
+    Button btn2Web, but2Calendar, btn2Map;
+    Button btn3Web, but3Calendar, btn3Map;
     TextView textViewStatus;
 
     @Override
@@ -35,8 +40,30 @@ public class MainActivity extends AppCompatActivity {
         btn1Web = (Button) findViewById(R.id.button1WebInfo);
         btn1Web.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
-                textViewStatus.setText("Code should display website for performance 1");
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d141080384%26view%3devent%26-childview%3d");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startIntent(webIntent);
+                textViewStatus.setText("Code should display website for event 1");
+            }
+        });
+
+        btn2Web = (Button) findViewById(R.id.button2WebInfo);
+        btn2Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d140362933%26view%3devent%26-childview%3d");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startIntent(webIntent);
+                textViewStatus.setText("Code should display website for event 2");
+            }
+        });
+
+        btn3Web = (Button) findViewById(R.id.button3WebInfo);
+        btn3Web.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri webpage = Uri.parse("http://www.css.edu/about/spotlight-arts-and-lectures/calendar.html?trumbaEmbed=eventid%3d134006157%26view%3devent%26-childview%3d");
+                Intent webIntent = new Intent(Intent.ACTION_VIEW, webpage);
+                startIntent(webIntent);
+                textViewStatus.setText("Code should display website for event 2");
             }
         });
 
@@ -51,7 +78,7 @@ public class MainActivity extends AppCompatActivity {
                 textViewStatus.setText("Code should set a calendar event for performance 1");
                 Intent calIntent = new Intent(Intent.ACTION_INSERT);
                 calIntent.setType("vnd.android.cursor.item/event");
-                calIntent.putExtra(CalendarContract.Events.TITLE, "SAL Colloquium 'The Robot Next Door'");
+                calIntent.putExtra(CalendarContract.Events.TITLE, getResources().getString(R.string.event1));
                 calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
                 GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
                 GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
@@ -59,8 +86,45 @@ public class MainActivity extends AppCompatActivity {
                         begDate.getTimeInMillis());
                 calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
                         endDate.getTimeInMillis());
-                startActivity(calIntent);
+                startIntent(calIntent);
+            }
+        });
 
+        but2Calendar = (Button) findViewById(R.id.button2Calendar);
+        but2Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 2");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, getResources().getString(R.string.event2));
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startIntent(calIntent);
+            }
+        });
+
+        but3Calendar = (Button) findViewById(R.id.button3Calendar);
+        but3Calendar.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                // Do something in response to button click
+                textViewStatus.setText("Code should set a calendar event for performance 3");
+                Intent calIntent = new Intent(Intent.ACTION_INSERT);
+                calIntent.setType("vnd.android.cursor.item/event");
+                calIntent.putExtra(CalendarContract.Events.TITLE, getResources().getString(R.string.event3));
+                calIntent.putExtra(CalendarContract.Events.EVENT_LOCATION, "Tower Hall 4119");
+                GregorianCalendar begDate = new GregorianCalendar(2017, 2, 24, 3, 40);
+                GregorianCalendar endDate = new GregorianCalendar(2017, 2, 24, 4, 40);
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_BEGIN_TIME,
+                        begDate.getTimeInMillis());
+                calIntent.putExtra(CalendarContract.EXTRA_EVENT_END_TIME,
+                        endDate.getTimeInMillis());
+                startIntent(calIntent);
             }
         });
 
@@ -70,13 +134,40 @@ public class MainActivity extends AppCompatActivity {
         btn1Map = (Button) findViewById(R.id.button1map);
         btn1Map.setOnClickListener(new View.OnClickListener() {
             public void onClick(View v) {
-                // Do something in response to button click
+                Uri location = Uri.parse("geo:0,0?q=1200+Kenwood+Ave,+Duluth,+MN+55811");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startIntent(mapIntent);
                 textViewStatus.setText("Code should display map for performance 1");
             }
         });
 
+        btn2Map = (Button) findViewById(R.id.button2map);
+        btn2Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri location = Uri.parse("geo:0,0?q=1200+Kenwood+Ave,+Duluth,+MN+55811");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startIntent(mapIntent);
+                textViewStatus.setText("Code should display map for performance 2");
+            }
+        });
+
+        btn3Map = (Button) findViewById(R.id.button3map);
+        btn3Map.setOnClickListener(new View.OnClickListener() {
+            public void onClick(View v) {
+                Uri location = Uri.parse("geo:0,0?q=1200+Kenwood+Ave,+Duluth,+MN+55811");
+                Intent mapIntent = new Intent(Intent.ACTION_VIEW, location);
+                startIntent(mapIntent);
+                textViewStatus.setText("Code should display map for performance 3");
+            }
+        });
 
     }
 
-
+    public void startIntent(Intent i) {
+        PackageManager pm = getPackageManager();
+        List activities = pm.queryIntentActivities(i, PackageManager.MATCH_DEFAULT_ONLY);
+        if (activities.size() > 0) {
+            startActivity(i);
+        }
+    }
 }
